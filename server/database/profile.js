dcf8f343dd56f4a610fe41053a1c775792fb2a10
RamTheBeginner <venkatakrishnas2003@gmail.com>
2024-04-21 02:22:56 +0530

    Added New Pages

--- server/database/profile.js
+++ server/database/profile.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 
 const profileSchema = new mongoose.Schema({
+  token:{type:String,required:true},
   image: { type: String, required: true }, 
   gender: { type: String, required: true }, 
   bio: { type: String, required: true }
