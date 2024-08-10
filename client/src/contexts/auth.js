c09b42e61167e11e928bc80bed170c22a4881e17
RamTheBeginner <venkatakrishnas2003@gmail.com>
2024-08-10 21:52:40 +0530

    Changed the Auth Page

--- client/src/contexts/auth.js
+++ client/src/contexts/auth.js
@@ -1,6 +1,3 @@
-
-
-
 import React, { useContext, useState, useEffect } from "react";
 import { auth } from "../config/firebase";
 // import { GoogleAuthProvider } from "firebase/auth";
@@ -9,7 +6,6 @@ import { onAuthStateChanged } from "firebase/auth";
 const AuthContext = React.createContext();
 
 export function useAuth() {
- 
   return useContext(AuthContext);
 }
 
@@ -21,35 +17,31 @@ export function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
- 
-  
     const unsubscribe = onAuthStateChanged(auth, initializeUser);
     return unsubscribe;
   }, []);
 
   async function initializeUser(user) {
-    
     if (user) {
-     console.log(user.email);
+      console.log(user.email);
       setCurrentUser({ ...user });
 
       // check if provider is email and password login
-      
+
       setIsEmailUser(user.email);
-      
 
       // check if the auth provider is google or not
-    //   const isGoogle = user.providerData.some(
-    //     (provider) => provider.providerId === GoogleAuthProvider.PROVIDER_ID
-    //   );
-    //   setIsGoogleUser(isGoogle);
+      //   const isGoogle = user.providerData.some(
+      //     (provider) => provider.providerId === GoogleAuthProvider.PROVIDER_ID
+      //   );
+      //   setIsGoogleUser(isGoogle);
 
       setUserLoggedIn(true);
     } else {
       setCurrentUser(null);
       setUserLoggedIn(false);
     }
-    
+
     setLoading(false);
   }
 
@@ -58,10 +50,8 @@ export function AuthProvider({ children }) {
     isEmailUser,
     isGoogleUser,
     currentUser,
-    setCurrentUser
+    setCurrentUser,
   };
- 
-  
 
   return (
     <AuthContext.Provider value={value}>
