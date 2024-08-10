fb37661242e999a3ba340c4635d1c49080ebeb46
Vishal <saivishalbellamkonda@gmail.com>
2024-08-11 01:48:59 +0530

    card the features Cards the integrated with the landing page

--- client/src/pages/landing/index.jsx
+++ client/src/pages/landing/index.jsx
@@ -1,12 +1,20 @@
 
 import React from 'react';
 import ControlledCarousel from './components/carousel';
+import Cards from './components/Card';
 
 
 function LandingPage() {
   return (
-    <div className="mt-10 h-50%">
+    <div className=" h-50%">
    <ControlledCarousel/>
+   <div className="justify-center items-center flex py-4 text-blue-500 font-weight-900 text-6xl font-bold animate-slide-up">
+              <span>Our Services</span>
+   </div>
+   <div className='mt-10'>
+   <Cards/>
+   </div>
+   
     </div>
   );
 }
