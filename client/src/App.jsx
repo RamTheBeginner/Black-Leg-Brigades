46086601e033381b723a93fceeafe6225217f1b3
Vishal <saivishalbellamkonda@gmail.com>
2024-08-10 20:24:47 +0530

    Landing page updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./pages/nav";
 import Home from "./pages/home";
 import About from "./pages/about";
+import LandingPage from "./pages/landing";
 const App = () => {
   return (
     <Router>
       <Nav />
+      <LandingPage/>
       <Routes>
         <Route path="/" element={<Home />} />
         {/*<Route path="/profile" element={<Profile />} />*/}
