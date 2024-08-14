88a34d5c33b4c0dcb52df18f90c4fbbca9adb00d
RamTheBeginner <venkatakrishnas2003@gmail.com>
2024-08-14 13:22:44 +0530

    Added the Charts feature and changed its UI

--- client/src/store/store.jsx
+++ client/src/store/store.jsx
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import dashboardReducer from './reducers/DasboardSlice';
 import userReducer from './reducers/UserSlice';
-
+import valueReducer from './reducers/ChartSlice'
 export default configureStore({
   reducer: {
     switcher: dashboardReducer,
     user: userReducer,
+    chart: valueReducer
   },
 });
